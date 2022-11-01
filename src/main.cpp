b193f25902eab6653a7ec3059eabd93da2c8d936
xTechon <DanielHiromoto@outlook.com>
2022-11-01 16:23:00 -0400

    Create CMakeLists.txt and init project

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include "imgui.h"
+
+int main() {
+  ImGui::Text("Hello, world %d", 123);
+  // if (ImGui::Button("Save"))
+  //  MySaveFunction();
+  //  ImGui::InputText("string", buf, IM_ARRAYSIZE(buf));
+  // ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
+  return 0;
+}
