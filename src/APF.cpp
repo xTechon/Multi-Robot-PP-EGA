fcb5052a7ee33a8d73f9603a71e4b62783d02c8f
xTechon <DanielHiromoto@outlook.com>
2022-11-02 16:07:58 -0400

    delete old code

--- src/APF.cpp
+++ src/APF.cpp
@@ -1,17 +1,1 @@
 #include "APF.h"
-
-int Grid::getWidth() { return width; }
-int Grid::getHeight() { return height; }
-
-// default values for a grid size
-Grid::Grid(void) {
-  this->width = 10;
-  this->height = 10;
-}
-
-Grid::Grid(int w, int h) {
-  this->width = w;
-  this->height = h;
-  this->map = std::vector<std::vector<std::vector<int>>>(
-      width, std::vector<std::vector<int>>(height, std::vector<int>(2, 0)));
-}
