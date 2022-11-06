cfbafdbde2370e2f811e8ec76376cd46ba806fb9
xTechon <DanielHiromoto@outlook.com>
2022-11-05 22:25:35 -0400

    Move grid.cpp tests to testGrid.cpp

--- test/test.cpp
+++ test/test.cpp
@@ -2,20 +2,3 @@
 #include <cstdint>
 
 #include "../src/APF.h"
-
-TEST_CASE("Grid Class functions") {
-  Grid *sqrRoom = new Grid(10, 10);
-  Grid *recRoom = new Grid(5, 10);
-
-  REQUIRE(sqrRoom->getWidth() == 10);
-  REQUIRE(sqrRoom->getHeight() == 10);
-  REQUIRE(recRoom->getWidth() == 5);
-  REQUIRE(recRoom->getHeight() == 10);
-
-  SECTION("Grid sets values") {
-    sqrRoom->setObstacle(9, 9);
-    recRoom->setObstacle(4, 9);
-    REQUIRE(sqrRoom->map[9][9][0] == 1);
-    REQUIRE(recRoom->map[4][9][0] == 1);
-  }
-}
