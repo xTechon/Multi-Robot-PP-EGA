01c99e950eaff804cd89f56bfff1812a5d3c8033
xTechon <DanielHiromoto@outlook.com>
2022-11-15 17:16:15 -0500

    Add tests for file.cpp
     - in order for the test to pass, it must be run from the project root
    directory
     - not enough time to find a novel way to unit test file paths

---
+++ test/testFile.cpp
@@ -0,0 +1,43 @@
+#include <catch2/catch_test_macros.hpp>
+#include <cstdint>
+
+#include "../src/APF.h"
+
+TEST_CASE("Test filepath") {
+  // set the structure to compare against
+  Grid *exampleMap = new Grid(4, 4);
+  exampleMap->setObstacle(0, 0);
+  exampleMap->setObstacle(1, 1);
+  exampleMap->setObstacle(2, 2);
+  exampleMap->setObstacle(3, 3);
+  exampleMap->setObstacle(0, 3);
+
+  Grid *fileImport = (Grid *)nullptr;
+
+  FileHandler *handler = new FileHandler();
+
+  std::string wrongPath = "./exampleMap.txt";
+  std::string correctPath = "./maps/exampleGrid.txt";
+
+  // test an invalid path
+  SECTION("Test an invlaid path") {
+    fileImport = handler->importGrid(&wrongPath, fileImport);
+
+    REQUIRE(fileImport == (Grid *)nullptr);
+  }
+
+  fileImport = handler->importGrid(&correctPath, fileImport);
+  // test a valid path
+  SECTION("Test a valid path") { REQUIRE(fileImport != (Grid *)nullptr); }
+
+  // Test comparison works properly
+  SECTION("Test map comparison works correctly") {
+    // correct map
+    REQUIRE(fileImport->map == exampleMap->map);
+
+    // change comparison map
+    exampleMap->setObstacle(1, 3);
+    // incorrect map
+    REQUIRE(fileImport->map != exampleMap->map);
+  }
+}
