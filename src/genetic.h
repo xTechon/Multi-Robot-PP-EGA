ade665b0075d571fbb3b554cc4b73c250ec9f054
xTechon <DanielHiromoto@outlook.com>
2022-11-24 13:57:05 -0500

    Create Header Structure for genetric.cpp

--- src/genetic.h
+++ src/genetic.h
@@ -1,4 +1,18 @@
+#import "grid.h"
+#include <random>
+#include <tuple>
+
 class Genetic {
+  Grid *terrain; // reference to grid for other functions
+  std::vector<std::vector<std::pair<int, int>>> potPaths;
+  std::vector<std::pair<int, int>> initPath;
+  std::pair<int, int> start;
+  std::pair<int, int> dest;
+  void genInitPath();
+
 public:
-  Genetic(void);
+  Genetic(Grid *grid);
+  void crossover();
+  void deletion();
+  void mutation();
 };
